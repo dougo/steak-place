54a079c0097ad88e9d8dfc66626084bd752ea6b6
Doug Orleans <dougorleans@gmail.com>
2016-07-13 23:42:36 -0400

    Add config settings still needed by sprockets-rails 2.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,16 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+
+# TODO: this defaults to true in sprockets-rails 3.0, but opal-rails still depends on <3.0
+# See https://github.com/opal/opal-rails/issues/82
+# Asset digests allow you to set far-future HTTP expiration dates on all assets,
+# yet still be able to expire them through the digest params.
+Rails.application.config.assets.digest = true
+
+# TODO: this defaults to true in sprockets-rails 3.0, but opal-rails still depends on <3.0
+# Adds additional error checking when serving assets at runtime.
+# Checks for improperly declared sprockets dependencies.
+# Raises helpful error messages.
+Rails.application.config.assets.raise_runtime_errors = true
